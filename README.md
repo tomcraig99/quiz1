6ad41a14f5a736f1b0c3bc6323886d4fa9aa332a
Johnny Bhojwani <johnnyvb@gmail.com>
2022-02-13 22:11:04 -0600

    project Quiz I - Initialized

---
+++ README.md
@@ -0,0 +1,9 @@
+This project is for Quiz I of the Spring Semester 2022.
+
+You are required to clone the repo, work the problem
+and push changes/updates to your repo with appropriate comments
+
+You are required to create a virtual environment (VE), activate the virtual environment
+and install the library - Matplotlib
+
+Make sure your local and GitHub repos do not contain your VE
---
+++ processStudents.py
@@ -0,0 +1,78 @@
+''' 
+*   Professor B would like to know which of his student have a GPA below 3.0.
+    To accomplish this, read the file - students.csv into the program. The program
+    should evaluate the GPA to see if it is higher or lower than 3.0. If it is,
+    then it should be written out to the file - processedStudents.csv. (This file
+    already contains headers and the headers should NOT be overwritten.) 
+
+*   Create a dictionary of each student where the student ID is the key
+    and the GPA is the value.
+
+*  print out the dictionary
+
+*  print out the corresponding GPA for student - 567890123
+
+I have outlined comments for each step of the program. You are
+not required to use them but it is provided to help you work
+through the logic of the problem.
+
+
+'''
+
+
+import csv
+
+
+# create a file object to open the file in read mode
+
+
+
+# create a csv object from the file object
+
+
+#skip the header row
+
+
+#create an outfile object for the pocessed record
+
+
+
+#create a new dictionary named 'student_dict'
+
+
+
+#use a loop to iterate through each row of the file
+
+    #check if the GPA is below 3.0. If so, write the record to the outfile
+    
+        
+
+
+
+    # append the record to the dictionary with the student id as the Key
+    # and the value as the GPA
+    
+
+
+
+
+
+#print the entire dictionary
+
+
+#Print the student id 
+
+
+#print out the corresponding GPA from the dictionary
+
+
+
+#close the outfile
+
+
+
+
+
+
+
+
