d553a569eed99c3b35d0533c268ed798e8e59eeb
Thomas Craig <tomcraig99@gmail.com>
2022-02-14 15:10:18 -0600

    completed quiz 1

--- processStudents.py
+++ processStudents.py
@@ -1,4 +1,4 @@
-''' 
+""" 
 *   Professor B would like to know which of his student have a GPA below 3.0.
     To accomplish this, read the file - students.csv into the program. The program
     should evaluate the GPA to see if it is higher or lower than 3.0. If it is,
@@ -17,62 +17,49 @@ not required to use them but it is provided to help you work
 through the logic of the problem.
 
 
-'''
+"""
 
 
 import csv
 
 
 # create a file object to open the file in read mode
-
+students = open("students.csv", "r")
 
 
 # create a csv object from the file object
+students_file = csv.reader(students, delimiter=",")
+
+# skip the header row
+next(students_file)
+
+# create an outfile object for the pocessed record
+processed = open("processedStudents.csv", "a", newline="")
+processed_file = csv.writer(processed, delimiter=",")
+
+# create a new dictionary named 'student_dict'
+student_dict = {}
 
 
-#skip the header row
-
-
-#create an outfile object for the pocessed record
-
-
-
-#create a new dictionary named 'student_dict'
-
-
-
-#use a loop to iterate through each row of the file
-
-    #check if the GPA is below 3.0. If so, write the record to the outfile
-    
-        
-
-
+# use a loop to iterate through each row of the file
+for row in students_file:
+    # check if the GPA is below 3.0. If so, write the record to the outfile
+    if float(row[8]) < 3.0:
+        processed_file.writerow(row)
 
     # append the record to the dictionary with the student id as the Key
     # and the value as the GPA
-    
+    student_dict[row[0]] = row[8]
 
 
+# print the entire dictionary
+print(student_dict)
 
+# Print the student id
+print("567890123")
 
+# print out the corresponding GPA from the dictionary
+print(student_dict["567890123"])
 
-#print the entire dictionary
-
-
-#Print the student id 
-
-
-#print out the corresponding GPA from the dictionary
-
-
-
-#close the outfile
-
-
-
-
-
-
-
-
+# close the outfile
+processed.close()
